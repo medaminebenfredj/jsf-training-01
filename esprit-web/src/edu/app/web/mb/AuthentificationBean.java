006b1757cddb29b622fa8d5ebabe65c4d48a8fd7
medalibettaieb <medali.bettaieb@esprit.ens.tn>
2013-04-15 18:06:50 +0100

    final version whith error message
    Signed-off-by: medalibettaieb <medali.bettaieb@esprit.ens.tn>

--- esprit-web/src/edu/app/web/mb/AuthentificationBean.java
+++ esprit-web/src/edu/app/web/mb/AuthentificationBean.java
@@ -3,8 +3,10 @@ package edu.app.web.mb;
 import java.io.Serializable;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import edu.app.business.AuthenticationServiceLocal;
 import edu.app.persistence.User;
@@ -19,6 +21,7 @@ public class AuthentificationBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private User user = new User();
+	private boolean loggedIn = false;
 	// injection
 	@EJB
 	private AuthenticationServiceLocal authenticationServiceLocal;
@@ -30,12 +33,25 @@ public class AuthentificationBean implements Serializable {
 				user.getLogin(), user.getPassword());
 		if (userFound != null) {
 			user = userFound;
+			loggedIn = true;
 			navigateTo = "/pages/admin/home";
 
 		} else {
-			navigateTo = "/pages/error";
+			loggedIn = false;
+			FacesMessage message = new FacesMessage("bad credentials ");
+			FacesContext.getCurrentInstance().addMessage(
+					"login_form:login_submit", message);
+			navigateTo = "";
 
 		}
+		return navigateTo;
+	}
+
+	public String logout() {
+		String navigateTo = null;
+		loggedIn = false;
+		user = new User();
+		navigateTo = "/welcome";
 		return navigateTo;
 	}
 
@@ -52,6 +68,14 @@ public class AuthentificationBean implements Serializable {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
 	}
 
 }
